3a28881b01760da02288bef8ff64d7ecd6111857
Bharath Reddy Madha <54592001+2711-bharath@users.noreply.github.com>
2024-02-03 21:37:32 +0530

    fix💄: Updated .prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
   singleQuote: true,
-  trailingComma: 'all',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+  bracketSpacing: true,
 };
